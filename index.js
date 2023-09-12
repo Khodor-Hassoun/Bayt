3905574cd06859e03473ef45501b413eefac9d04
Khodor Hassoun <khodorhassoun@outlook.com>
2023-09-13 00:37:29 +0300

    Solve exercise 3

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hello");
+const nestedObject = {
+  undef: undefined,
+  nul: null,
+  data: {
+    info: {
+      stuff: {
+        thing: {
+          moreStuff: {
+            magicNumber: 44,
+            something: "foo2",
+          },
+        },
+      },
+    },
+  },
+  number: 1,
+  arr: [1, 2, 3],
+  currentDate: new Date(),
+};
+let i = 0;
+function contains(object, value) {
+  let values = Object.values(object);
+  for (let val of values) {
+    if (val === value) {
+      return true;
+    }
+    if (typeof val === "object" && val != null) {
+      if (contains(val, value)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
