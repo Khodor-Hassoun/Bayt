fed5ece26bcdaf2426249d04895621ed193a9bcc
Khodor Hassoun <khodorhassoun@outlook.com>
2023-09-15 13:13:02 +0300

    Minor updates

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const navigationDropDownList = document.querySelector(
 // Variable to scroll up
 const scrollUpBtn = document.querySelector(".footer-up");
 
+// Variable for form
+const contactForm = document.querySelector(".section-contact-form");
+
 // Add or remove class to display the dropdown menu
 dropDownBtm.addEventListener("click", (e) => {
   navigationDropDownList.classList.toggle("navigation-display-hide");
@@ -28,4 +31,11 @@ scrollUpBtn.addEventListener("click", (e) => {
     top: 0,
     behavior: "smooth",
   });
+});
+
+// Log the form data
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(contactForm);
+  console.log(formData.get("checkbox") === "on");
 });
