e3528e67277d3659e7a2cd26a6daf5405b064bd6
Khodor Hassoun <khodorhassoun@outlook.com>
2023-10-08 13:24:12 +0300

    Solve sort array in tcl

--- phase_three_exercises/tcl_exercise/index.tcl
+++ phase_three_exercises/tcl_exercise/index.tcl
@@ -1,7 +1,8 @@
 # puts "Hello World"
 # puts "Second line"
 
-set list {1 2 3 4}
+set listToSort {3 6 8 7 0 1 4 2 9 5}
+set stringToReverse "TCL is a Tool Command Language"
 # puts $list
 
 proc reverse {str} {
@@ -15,4 +16,21 @@ proc reverse {str} {
 
 }
 
-puts "List: $list is reversed to [reverse $list]"
+proc sortedList {num} {
+    for {set i 0} {$i < [llength $num] } {incr i} {
+        for {set j $i} {$j < [llength $num] } {incr j} {
+            if {[lindex $num $j] < [lindex $num $i]} {
+                set temp [lindex $num $i]
+                set num [lset num $i [lindex $num $j]]
+                set num [lset num $j $temp]
+            }
+        }
+    }
+
+    return $num
+
+}
+
+puts "String: $stringToReverse, is reversed to\n[reverse $stringToReverse]"
+puts \n
+puts "List: $listToSort, is sorted to\n[sortedList $listToSort]"
